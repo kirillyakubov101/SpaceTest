566c16a3a674c801a891e6d96afa9660d00d4d7d
kirillyakubov <kirillyakubov101@gmail.com>
2020-04-27 13:23:00 -0400

    Lecture 61

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent] //sick method. only one script is allowed
+
+public class Oscillator : MonoBehaviour
+{
+	[SerializeField] Vector3 movementVector;
+	[Range(0, 1)] [SerializeField] float movementFactor;
+
+	Vector3 startingPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		startingPos = transform.position;
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		Vector3 Offset = movementVector * movementFactor;
+		transform.position = startingPos + Offset;
+    }
+}
